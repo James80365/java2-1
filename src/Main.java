06a32b6bad26017d9572203ccba78084290bebcb
James <james80365@gmail.com>
2023-02-13 13:31:52 +0300

    main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 20_000; // стоимость билета
+
+        int amountmiles = 20; // стоимость мили
+
+        System.out.println((amount / amountmiles) + " бонусные мили");
+
+    }
+}
